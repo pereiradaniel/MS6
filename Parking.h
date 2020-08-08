5be2ef28b12f646108d1dcac9833305c9835c4b7
Daniel Paul Grech Pereira <13129650+pereiradaniel@users.noreply.github.com>
2020-08-08 11:45:40 -0400

    Comment and update Parking.h - Add comments showing new functions and things to be modified or not.

--- Parking.h
+++ Parking.h
@@ -1,6 +1,7 @@
 #ifndef SDDS_PARKING_H__
 #define SDDS_PARKING_H__
 #include "Menu.h"
+#include "Vehicle.h"
 
 namespace sdds {
 	const int	MAX_LOT_SIZE = 100;
@@ -9,23 +10,30 @@ namespace sdds {
 		char* m_datafile;				// may be nullptr -> Parking in empty state
 		Menu	m_parkingMenu;
 		Menu	m_vehicleMenu;
+		int		m_litSize;
+		int		m_Vcnt;
+		Vehicle* m_parkingSpots[MAX_LOT_SIZE];
 	private:
-		bool	isEmpty() const;
-		void	setEmpty();
-		void	parkingStatus() const;	// message->****** Seneca Valet Parking ******
-		void	parkVehicle();
-		void	returnVehicle();		// message->Returning Vehicle
-		void	listParkedVehicles();	// message->Listing Parked Vehicles
-		bool	closeParking();			// message->Closing Parking
-		bool	exitParkingApp();
-		bool	load();
-		void	save();
+		bool	isEmpty() const;				// no changes
+		void	setEmpty();						// no changes
+		void	parkingStatus() const;			// modify
+		void	parkVehicle();					// modify
+		void	returnVehicle();				// modify
+		void	listParkedVehicles();			// modify
+		bool	closeParking();					// modify
+		bool	exitParkingApp();				// no changes
+		bool	load();							// modify
+		void	save();							// modify
+		bool moveToParkingSpot(Vehicle* V);		// new
+		void endOfDay();						// new
+		int findVehicle(const char* licPlate);	// new
+		Vehicle* getVehicle() const;			// new
 	public:
-		Parking(const char* datafile);
+		Parking(const char* datafile, int lotSize);		// modified
 		Parking(const Parking&) = delete;
 		void operator=(const Parking&) = delete;
-		int run();
-		~Parking();
+		int run();										// no changes
+		~Parking();										// modified
 	};
 }
 #endif
